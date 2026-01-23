2dcecec95f1e8a0bb0db454dbd9efc3715547bd7
micaeladuarte2santos <micaelaangelesduarte@gmail.com>
2026-01-23 17:42:45 -0300

    permite abrir archivos con variables de entorno .env

--- src/main/java/com/perfumeria/SistemaGestionApplication.java
+++ src/main/java/com/perfumeria/SistemaGestionApplication.java
@@ -1,5 +1,6 @@
 package com.perfumeria;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemaGestionApplication {
 
 	public static void main(String[] args) {
+		// cargar variables de entorno desde el .env
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> 
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+		
 		SpringApplication.run(SistemaGestionApplication.class, args);
 	}
 
