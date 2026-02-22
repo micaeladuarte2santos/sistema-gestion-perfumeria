6fe5d6bd4f6f418dcd0d7b115882cc1c8c383e1e
micaeladuarte2santos <micaelaangelesduarte@gmail.com>
2026-02-22 20:00:55 -0300

    fix CORS

--- src/main/java/com/perfumeria/config/WebConfig.java
+++ src/main/java/com/perfumeria/config/WebConfig.java
@@ -1,21 +1,11 @@
 package com.perfumeria.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // Define explícitamente el origen permitido
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Cambia esto al origen correcto
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true); // Permitir el uso de credenciales
-    }
+    // La configuración CORS se maneja en SecurityConfig
 }
     
 
